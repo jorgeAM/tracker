39fc16d459bec5dcce834bd5d0f2fd2a3dc94f11
Jorge L <jorge.alfmur@gmail.com>
2018-01-30 22:33:46 -0500

    refactor de obtención y subida a firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { UsuarioProvider } from "../../providers/usuario/usuario";
 import { UbicacionProvider } from '../../providers/ubicacion/ubicacion';
 
 @Component({
@@ -7,15 +9,28 @@ import { UbicacionProvider } from '../../providers/ubicacion/ubicacion';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  map:any;
+  usuario:any;
 
   constructor(
     public navCtrl: NavController,
+    private afDB: AngularFireDatabase,
+    private usuarioService: UsuarioProvider,
     private ubicacionService: UbicacionProvider
-  ) { }
+  ) {
+    this.usuario = this.afDB.object('/usuarios/'+this.usuarioService.clave);
+  }
 
   ionViewDidLoad(){
-    this.ubicacionService.getPosicion();
+    this.ubicacionService.getPosicion()
+      .subscribe(data => {
+        console.log(data);
+
+        this.usuario.update({
+          latitud: data.coords.latitude,
+          longitud: data.coords.longitude
+        });
+
+      });
   }
 
 }
--- src/providers/ubicacion/ubicacion.ts
+++ src/providers/ubicacion/ubicacion.ts
@@ -1,32 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Geolocation, Geoposition } from '@ionic-native/geolocation';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Geolocation } from '@ionic-native/geolocation';
 import { UsuarioProvider } from "../usuario/usuario";
 
 @Injectable()
 export class UbicacionProvider {
-  usuario: any;
 
   constructor(
     private geolocation: Geolocation,
-    private afDB: AngularFireDatabase,
-    private usuarioService: UsuarioProvider
-  ) {
-    this.usuario = this.afDB.object('usuarios/'+this.usuarioService.clave)
-  }
+  ) { }
 
-  getPosicion():any{
-    let watch = this.geolocation.watchPosition();
-    watch.subscribe((data) => {
-      // data can be a set of coordinates, or an error (if an error occurred).
-      // data.coords.latitude
-      // data.coords.longitude
-      console.log(data);
-      this.usuario.update({
-        latitud: data.coords.latitude,
-        longitud: data.coords.longitude
-      })
-    });
+  getPosicion(){
+    return this.geolocation.watchPosition();
   }
 
 }
